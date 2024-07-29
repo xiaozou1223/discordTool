1d68251ecc65be28c031600f20e26606573dcb84
XiaoZou <l54754712@yahoo.com>
2024-07-29 16:18:51 +0800

    允許跨域

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import * as cookieParser from 'cookie-parser';
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+const port = Number(process.env.BACKEND_PORT) || 8088;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  app.use(cookieParser());
+  await app.listen(port);
+  console.log(`run in ${port}`);
 }
 bootstrap();
