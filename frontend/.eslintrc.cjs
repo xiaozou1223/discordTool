b323bbcb7864c9df584f2e04bab65c469c6cf6cf
XiaoZou <l54754712@yahoo.com>
2024-07-30 11:19:59 +0800

    格式化相關設定

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+    rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'max-len': ['error', { code: 150, tabWidth: 2, ignoreUrls: true }],
+  },
+}
