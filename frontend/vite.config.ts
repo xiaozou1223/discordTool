2fb7c02e1263602111530d6fbb8511042dc4b9d8
XiaoZou <l54754712@yahoo.com>
2024-08-11 01:41:41 +0800

    安裝PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'DiscordTool',
+        short_name: 'DiscordTool',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#343a40',
+        icons: [
+          {
+            src: 'https://i.imgur.com/Yow9X0v.jpeg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'https://i.imgur.com/Yow9X0v.jpeg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -18,4 +42,4 @@ export default defineConfig({
   define: {
     'process.env': process.env,
   },
-});
+})
