647f9a250cee37a267c9f24f1a0f92f9d69f7844
XiaoZou <l54754712@yahoo.com>
2024-08-11 00:33:50 +0800

    增加處裡jwt 並重新命名function

--- frontend/src/components/User.ts
+++ frontend/src/components/User.ts
@@ -1,16 +1,40 @@
-import { ReadUserResponseDto } from '@/api/user/dto/read-user.dto';
-import { ref } from 'vue';
+import { ReadUserResponseDto } from '@/api/user/dto/read-user.dto'
+import Cookies from 'js-cookie'
+import * as jwtDecode from 'jwt-decode'
+import { ref, type Ref } from 'vue'
 
-const user = ref(new ReadUserResponseDto());
+const user = ref(new ReadUserResponseDto())
+const jwt: Ref<string | undefined> = ref(undefined)
+const iconUrl: Ref<string> = ref('https://i.imgur.com/Yow9X0v.jpeg')
 
-const setUser = (newUser: ReadUserResponseDto) => {
-  console.log('setUser!')
-  user.value = newUser;
-};
+const reloadUser = () => {
+  console.log('reloadUser!')
+  if (jwt.value) {
+    user.value = jwtDecode.jwtDecode(jwt.value)
+    if (user.value.discordUserData && user.value.discordUserData.avatar) {
+      iconUrl.value = `https://cdn.discordapp.com/avatars/${user.value.discordUserData.id}/${user.value.discordUserData.avatar}.png`
+    } else {
+      iconUrl.value = 'https://i.imgur.com/Yow9X0v.jpeg'
+    }
+  }
+}
+
+const reloadJwt = () => {
+  console.log('reloadJwt!')
+  jwt.value = Cookies.get('jwt')
+}
 
 export function UserStore() {
   return {
+    iconUrl,
     user,
-    setUser,
-  };
+    reloadUser,
+  }
+}
+
+export function JwtStore() {
+  return {
+    jwt,
+    reloadJwt,
+  }
 }
