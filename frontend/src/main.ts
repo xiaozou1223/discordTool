d30864c4d2772da38a13fc7e383dd1c0f100b6fe
XiaoZou <l54754712@yahoo.com>
2024-08-11 01:59:49 +0800

    安裝PWA

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,9 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import './assets/bootstrap/css/bootstrap.min.css';
-import './assets/bootstrap/js/bootstrap.min.js';
-import './assets/js/bold-and-dark.js';
-
-createApp(App).use(router).mount('#app');
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/bootstrap/css/bootstrap.min.css'
+import './assets/bootstrap/js/bootstrap.min.js'
+import './assets/js/bold-and-dark.js'
+import { useRegisterSW } from 'virtual:pwa-register/vue'
+createApp(App).use(router).mount('#app')
+useRegisterSW()
