071320001ba3a95930a5ff53270ba961ecc00655
XiaoZou <l54754712@yahoo.com>
2024-08-11 14:21:31 +0800

    修正無法摺疊

--- frontend/src/components/AppNav.vue
+++ frontend/src/components/AppNav.vue
@@ -2,11 +2,6 @@
   <nav v-if="!jwt" class="navbar navbar-expand-md sticky-top py-3 navbar-dark" id="mainNav">
     <div class="container">
       <a class="navbar-brand d-flex align-items-center" href="/"><span>DiscordTool</span></a>
-      <div class="collapse navbar-collapse" id="navcol-1">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item"></li>
-        </ul>
-      </div>
     </div>
   </nav>
   <nav v-else class="navbar navbar-expand-md sticky-top py-3 navbar-dark" id="mainNav">
@@ -18,11 +13,11 @@
       </a>
       <span v-if="user.isTokenValid" class="navbar-text" style="font-size: 15px; color: rgb(0, 255, 71); font-weight: bold">Token有效</span>
       <span v-else class="navbar-text" style="font-size: 15px; color: rgb(255, 0, 0); font-weight: bold">Token無效</span>
-      <button class="navbar-toggler" @click="toggleNav()">
+      <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol">
         <span class="visually-hidden">Toggle navigation</span>
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse text-center" id="navcol-1" ref="navCollapse">
+      <div class="collapse navbar-collapse text-center" id="navcol">
         <ul class="navbar-nav mx-auto"></ul>
         <div class="container">
           <div class="row navbar-links">
@@ -31,6 +26,8 @@
                 v-if="user.isTokenValid"
                 role="button"
                 style="font-size: 22px; color: rgb(255, 255, 255); font-weight: bold"
+                data-bs-toggle="collapse"
+                data-bs-target="#navcol"
                 @click="switchRouter('DiscordServer')"
                 >伺服器</a
               >
@@ -40,12 +37,21 @@
                 v-if="user.isTokenValid"
                 role="button"
                 style="font-size: 22px; color: rgb(255, 255, 255); font-weight: bold"
+                data-bs-toggle="collapse"
+                data-bs-target="#navcol"
                 @click="switchRouter('User')"
                 >已監聽頻道</a
               >
             </div>
             <div class="col-md-3">
-              <a role="button" style="font-size: 22px; color: rgb(255, 255, 255); font-weight: bold" @click="switchRouter('User')">帳號設定</a>
+              <a
+                role="button"
+                style="font-size: 22px; color: rgb(255, 255, 255); font-weight: bold"
+                data-bs-toggle="collapse"
+                data-bs-target="#navcol"
+                @click="switchRouter('User')"
+                >帳號設定</a
+              >
             </div>
             <div class="col-md-3"><a role="button" style="font-size: 22px; color: rgb(255, 0, 0); font-weight: bold" @click="logout">登出</a></div>
           </div>
@@ -59,11 +65,9 @@ import router from '@/router'
 import Cookies from 'js-cookie'
 import { onMounted, ref, type Ref } from 'vue'
 import { UserStore, JwtStore, checkLoginStatus } from './User'
-import Collapse from 'bootstrap/js/dist/collapse'
 
 const { user, iconUrl, reloadUser } = UserStore()
 const { jwt, reloadJwt } = JwtStore()
-const navCollapse: Ref<HTMLDivElement | null> = ref(null)
 
 onMounted(() => {
   reloadJwt()
@@ -80,36 +84,10 @@ function logout() {
 
 function switchRouter(routerName: string) {
   router.push({ name: routerName })
-  if (navCollapse.value) {
-    const isExpanded = navCollapse.value.classList.contains('show')
-    const isTransitioning = navCollapse.value.classList.contains('collapsing')
-    if (isExpanded) {
-      const collapse = new Collapse(navCollapse.value)
-      collapse.hide()
-    } else if (isTransitioning) {
-      navCollapse.value!.addEventListener('shown.bs.collapse', handleShown)
-    }
-  }
-}
-
-function handleShown() {
-  if (!navCollapse.value) {
-    return
-  }
-  const collapse = new Collapse(navCollapse.value)
-  collapse.hide()
-  navCollapse.value.removeEventListener('shown.bs.collapse', handleShown)
-}
-
-function toggleNav() {
-  if (navCollapse.value) {
-    const collapse = new Collapse(navCollapse.value)
-    collapse.toggle()
-  }
 }
 </script>
 
-<style>
+<style scoped>
 span {
   user-select: none; /* For modern browsers */
   -webkit-user-select: none; /* For Safari */
