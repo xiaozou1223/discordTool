76d299fe7dfeddd6bae78b43ec4be808e816bbff
XiaoZou <l54754712@yahoo.com>
2024-08-14 11:15:44 +0800

    將Message顯示分離出來

--- frontend/src/components/ChannelInfoModal/MessageSearcher.vue
+++ frontend/src/components/ChannelInfoModal/MessageSearcher.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="row" style="margin-right: 0px; margin-left: 0px; margin-top: 10px">
-      <div class="col-1" style="text-align: center; padding-top: 28px; padding-bottom: 28px; font-weight: bolder">
+      <div class="col-2" style="text-align: center; padding-top: 28px; padding-bottom: 28px; font-weight: bolder">
         <span style="padding: 0px; margin-left: 10px; font-weight: bolder">從</span>
       </div>
       <div class="col-3" style="text-align: center; padding-top: 5px; padding-bottom: 5px">
@@ -31,25 +31,7 @@
       </div>
       <div class="row collapse" id="message-viewer" style="margin-right: 0px; margin-left: 0px; margin-top: 10px">
         <div class="container" style="max-height: 300px; overflow-y: auto; overflow-x: hidden; border: 1px solid #ccc" ref="messagesViewer">
-          <div class="row" v-for="message of searchMessageResult?.messages">
-            <div class="col-2" style="text-align: center; padding-top: 5px; padding-bottom: 5px">
-              <img class="rounded-circle" width="40px" height="40px" :src="generateUserAvatarUrl(message[0].author)" />
-            </div>
-            <div class="col" style="text-align: left; padding-top: 5px; padding-bottom: 5px">
-              <div class="row">
-                <span style="padding: 0px; margin-left: 10px; font-weight: bolder">{{ message[0].author.global_name }}</span>
-              </div>
-              <div class="row">
-                <div v-html="parseMessageContent(message[0].content)"></div>
-              </div>
-              <div class="row" v-for="attachment of message[0].attachments">
-                <img v-if="attachment.content_type === 'image/jpeg'" :src="attachment.url" />
-              </div>
-              <div class="row" v-for="sticker_item of message[0].sticker_items">
-                <img :src="generateStickerUrl(sticker_item)" :alt="sticker_item.name" />
-              </div>
-            </div>
-          </div>
+          <Message v-for="message of searchMessageResult.messages" :message="message[0]" />
         </div>
       </div>
     </div>
@@ -71,6 +53,7 @@ import type { APISearchMessage } from '@/common.class'
 import type { APIGuild, APIGuildMember, APIMessage, APIMessageComponent } from 'discord-api-types/v10'
 import { ref, type Ref, onMounted, watch, computed } from 'vue'
 import { generateUserAvatarUrl, generateGuildIconUrl, generateStickerUrl, generateEmojiUrl } from '../../functions/Discord'
+import Message from '../MessageViewer/Message.vue'
 
 interface FilterSetting {
   authorId: string
@@ -116,18 +99,6 @@ onMounted(async () => {
     console.log(searchMessageResult.value)
   }
 })
-
-function parseMessageContent(content: string) {
-  const emojiRegex = /<:[a-zA-Z0-9_]+:[0-9]+>/g
-
-  const formattedContent = content.replace(emojiRegex, (match) => {
-    const [_, emojiName, emojiId] = match.match(/^<:([a-zA-Z0-9_]+):([0-9]+)>$/)!
-    const emojiUrl = generateEmojiUrl(emojiId)
-    return `<img src="${emojiUrl}" alt="${emojiName}" class="emoji">`
-  })
-
-  return formattedContent.replace(/(^|>)([^<]+)(<|$)/g, '$1<span>$2</span>$3')
-}
 </script>
 
 <style>
--- frontend/src/components/MessageViewer/Message.vue
+++ frontend/src/components/MessageViewer/Message.vue
@@ -1,3 +1,39 @@
-<template></template>
+<template>
+  <div class="row">
+    <div class="col-2" style="text-align: center; padding-top: 5px; padding-bottom: 5px">
+      <img class="rounded-circle" width="40px" height="40px" :src="generateUserAvatarUrl(message.author)" />
+    </div>
+    <div class="col" style="text-align: left; padding-top: 5px; padding-bottom: 5px">
+      <div class="row">
+        <span style="padding: 0px; margin-left: 10px; font-weight: bolder">{{ message.author.global_name }}</span>
+      </div>
+      <div class="row">
+        <div v-html="parseMessageContent(message.content)"></div>
+      </div>
+      <div class="row" v-for="attachment of message.attachments">
+        <img v-if="attachment.content_type === 'image/jpeg'" :src="attachment.url" />
+      </div>
+      <div class="row" v-for="sticker_item of message.sticker_items">
+        <img :src="generateStickerUrl(sticker_item)" :alt="sticker_item.name" />
+      </div>
+    </div>
+  </div>
+</template>
 <script setup lang="ts">
+import type { APIMessage } from 'discord-api-types/v10'
+import { generateUserAvatarUrl, generateStickerUrl, generateEmojiUrl } from '../../functions/Discord'
+
+const props = defineProps<{ message: APIMessage }>()
+
+function parseMessageContent(content: string) {
+  const emojiRegex = /<:[a-zA-Z0-9_]+:[0-9]+>/g
+
+  const formattedContent = content.replace(emojiRegex, (match) => {
+    const [_, emojiName, emojiId] = match.match(/^<:([a-zA-Z0-9_]+):([0-9]+)>$/)!
+    const emojiUrl = generateEmojiUrl(emojiId)
+    return `<img src="${emojiUrl}" alt="${emojiName}" class="emoji">`
+  })
+
+  return formattedContent.replace(/(^|>)([^<]+)(<|$)/g, '$1<span>$2</span>$3')
+}
 </script>
