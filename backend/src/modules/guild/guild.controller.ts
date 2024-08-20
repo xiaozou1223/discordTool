6dac8482a597423f7b7e1786a23f582076f087a3
XiaoZou <l54754712@yahoo.com>
2024-08-20 21:10:55 +0800

    如果discord請求失敗的話 重新驗證token

--- backend/src/modules/guild/guild.controller.ts
+++ backend/src/modules/guild/guild.controller.ts
@@ -5,9 +5,12 @@ import { Response, Request, query } from 'express';
 import { ReadUserResponseDto } from '../user/dto/read-user.dto';
 import * as jwtDecode from 'jwt-decode';
 import { ApiResponse, APISearchMessage } from 'src/common.class';
-import { APIGuildMember, APIMessage, APIUser } from 'discord-api-types/v10';
+import { APIGuild, APIGuildMember, APIMessage, APIRole, APIUser } from 'discord-api-types/v10';
 import { channel } from 'process';
 import { ReadGuildsResponseDto } from './dto/read-guilds';
+import { HttpStatusCode } from 'axios';
+import { DiscordChannel } from './dto/read-channel';
+import { generateJwt } from 'src/jwt';
 
 @Controller('/api/guild')
 export class GuildController {
@@ -19,36 +22,54 @@ export class GuildController {
   @Get('/')
   async getGuilds(@Req() req: Request, @Res() res: Response) {
     const result: ApiResponse<ReadGuildsResponseDto[]> = await this.guildService.getGuilds(req.discordToken);
+    if (result.statusCode === HttpStatusCode.Unauthorized) {
+      await this.refreshDiscordTokenStatus(req, res);
+    }
     return res.status(result.statusCode).send(result);
   }
 
   @Get(':guildId/channels')
   async getChannels(@Param('guildId') guildId: string, @Req() req: Request, @Res() res: Response) {
-    const result = await this.guildService.getChannels(req.discordToken, guildId);
+    const result: ApiResponse<DiscordChannel[]> = await this.guildService.getChannels(req.discordToken, guildId);
+    if (result.statusCode === HttpStatusCode.Unauthorized) {
+      await this.refreshDiscordTokenStatus(req, res);
+    }
     return res.status(result.statusCode).send(result);
   }
 
   @Get(':guildId/members/:userId')
   async getMemberByUserIdAndGuildId(@Param('guildId') guildId: string, @Param('userId') userId: string, @Req() req: Request, @Res() res: Response) {
-    const result = await this.guildService.getMemberByUserIdAndGuildId(req.discordToken, guildId, userId);
+    const result: ApiResponse<APIGuildMember> = await this.guildService.getMemberByUserIdAndGuildId(req.discordToken, guildId, userId);
+    if (result.statusCode === HttpStatusCode.Unauthorized) {
+      await this.refreshDiscordTokenStatus(req, res);
+    }
     return res.status(result.statusCode).send(result);
   }
 
   @Get(':guildId/roles')
   async getRolesByUserIdAndGuildId(@Param('guildId') guildId: string, @Req() req: Request, @Res() res: Response) {
-    const result = await this.guildService.getRolesByGuildId(req.discordToken, guildId);
+    const result: ApiResponse<APIRole[]> = await this.guildService.getRolesByGuildId(req.discordToken, guildId);
+    if (result.statusCode === HttpStatusCode.Unauthorized) {
+      await this.refreshDiscordTokenStatus(req, res);
+    }
     return res.status(result.statusCode).send(result);
   }
 
   @Get(':guildId')
   async getGuildInfo(@Param('guildId') guildId: string, @Req() req: Request, @Res() res: Response) {
-    const result = await this.guildService.getGuildInfo(req.discordToken, guildId);
+    const result: ApiResponse<APIGuild> = await this.guildService.getGuildInfo(req.discordToken, guildId);
+    if (result.statusCode === HttpStatusCode.Unauthorized) {
+      await this.refreshDiscordTokenStatus(req, res);
+    }
     return res.status(result.statusCode).send(result);
   }
 
   @Get(':guildId/members/:discordUserId')
   async getUser(@Param('guildId') guildId: string, @Param('discordUserId') discordUserId: string, @Req() req: Request, @Res() res: Response) {
     const result: ApiResponse<APIGuildMember> = await this.guildService.getMember(req.discordToken, guildId, discordUserId);
+    if (result.statusCode === HttpStatusCode.Unauthorized) {
+      await this.refreshDiscordTokenStatus(req, res);
+    }
     return res.status(result.statusCode).send(result);
   }
 
@@ -56,12 +77,24 @@ export class GuildController {
   async searchMessage(@Param('guildId') guildId: string, @Query() query: string, @Req() req: Request, @Res() res: Response) {
     const queryString = req.originalUrl.split('?')[1];
     const result: ApiResponse<APISearchMessage> = await this.guildService.searchMessage(req.discordToken, guildId, queryString);
+    if (result.statusCode === HttpStatusCode.Unauthorized) {
+      await this.refreshDiscordTokenStatus(req, res);
+    }
     return res.status(result.statusCode).send(result);
   }
 
   @Delete(':channelId/:messageId')
   async deleteMessage(@Param('channelId') channelId: string, @Param('messageId') messageId: string, @Req() req: Request, @Res() res: Response) {
     const result: ApiResponse<null> = await this.guildService.deleteMessage(req.discordToken, channelId, messageId);
+    if (result.statusCode === HttpStatusCode.Unauthorized) {
+      await this.refreshDiscordTokenStatus(req, res);
+    }
     return res.status(result.statusCode).send(result);
+  }
+
+  async refreshDiscordTokenStatus(req: Request, res: Response) {
+    const updateUserResult = await this.userService.update(req.user.account, { discordToken: req.discordToken }, res);
+    const jwtResult = generateJwt(updateUserResult.data);
+    res.cookie('jwt', jwtResult.accessToken, { httpOnly: false, path: '/', secure: false });
   }
 }
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,8 +1,25 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios'
+import { HttpStatusCode } from 'axios'
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-});
-export default apiClient;
+})
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    const err = error as AxiosError
+    if (!err.config!.url?.startsWith('/api/login') && err.response && err.response.status === HttpStatusCode.Unauthorized) {
+      alert('權限驗證失敗，將進行重新整理')
+      window.location.reload()
+      return Promise.reject(new Error('權限驗證失敗，將進行重新整理!'))
+    }
+    return Promise.reject(err)
+  },
+)
+
+export default apiClient
